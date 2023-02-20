c92ae68361aebb7b337584a01c4f2da387354216
Annik <105221984+DorianSolaris@users.noreply.github.com>
2023-02-20 06:25:12 +0100

    Update decide.js

--- Decide for me/decide.js
+++ Decide for me/decide.js
@@ -1,13 +1,18 @@
 
-var randomNumber1 = Math.floor(Math.random() * 7) + 1;
-var randomNumber2 = Math.floor(Math.random()  +  1);
+let randomNumber1 = Math.floor(Math.random() * 7) + 1;
+let randomNumber2 = Math.floor(Math.random()  +  1);
+
+
+let input = document.querySelector("#input");
+let button = document.querySelector(".btn");
+button.disabled = true;
 
 
 
 function fortuneTell() {
   document.querySelector(".btn").addEventListener("click", function () {
    
-   var starsSign = document.querySelector(".img1").setAttribute("src", "images/Star" + randomNumber1 + ".png");
+   let starsSign = document.querySelector(".img1").setAttribute("src", "images/Star" + randomNumber1 + ".png");
   $("h3").css({"color": "#9b59b6", "text-shadow": "2px 0 #ecf0f1"});
    if (randomNumber1 % 2 === 0 && randomNumber2 % 2 !== 0) {
     return document.querySelector("h3").innerHTML = "Do it! Or terrible things will happen.", 
@@ -25,19 +30,29 @@ function fortuneTell() {
 fortuneTell()
 
 
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if(document.querySelector("#input").value === "") {
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+  }
+
 
 
 $("#old-btn").click(function () {
  $("#old-btn").remove();
- $(".buttons").before("<button class='btn' id='new-btn' onclick='pageReload()'>You can ask next question.</button>");
+ $(".buttons").before("<button class='btn' id='new-btn' onclick='pageReload()'>Do you want to ask next question.</button>");
+$("#new-btn").click(function pageReload() {
+  $(".sign").fadeOut();
+  document.location.reload();
+});
+
 });
 
 
 
-function pageReload() {
-  $(".sign").fadeOut();
-  document.location.reload();
-};
 
- 
 
